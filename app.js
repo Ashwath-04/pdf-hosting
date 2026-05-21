b027abac2ba33748133b0f9f4f5e9199c4b27b55
Ashwath-04 <ashwath.mohan.t@gmail.com>
2026-05-21 11:20:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+async function loadPDFs() {
+
+    const response = await fetch('pdfs.json');
+    const pdfs = await response.json();
+
+    const grid = document.getElementById('pdf-grid');
+
+    pdfs.forEach(pdf => {
+
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        card.innerHTML = `
+            <img src="${pdf.thumbnail}" alt="${pdf.title}">
+
+            <div class="card-content">
+                <h3>${pdf.title}</h3>
+
+                <a class="btn"
+                   href="viewer.html?file=${pdf.file}">
+                   View
+                </a>
+
+                <a class="btn btn-download"
+                   href="${pdf.file}"
+                   download>
+                   Download
+                </a>
+            </div>
+        `;
+
+        grid.appendChild(card);
+
+    });
+
+}
+
+loadPDFs();
